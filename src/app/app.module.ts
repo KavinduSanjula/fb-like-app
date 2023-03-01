66f813a0dcdb3e314e6aad6c427d318e84986dc7
Kavindu Sanjula Dasanayake <123143684+RavanaDevs@users.noreply.github.com>
2023-03-01 15:20:51 +0530

    Basic Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './components/pages/signup-page/signup-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const routes : Routes = [
+  {path:'', component:HomePageComponent},
+  {path:'sign-up', component:SignupPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    LoginPageComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
-  loggedIn : boolean = false;
+  loggedIn : boolean = true;
 
 }
